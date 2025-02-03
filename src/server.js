2593ddd27ade493b00a2016256ef453e277356d5
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-02-03 20:42:50 -0300

    Alterada estrutura para adicionar middleware de tratativa de erro

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
+require("express-async-errors")
 const express = require('express');
+const AppError = require('./utils/AppError');
 const app = express();
 const PORT = 3000;
-
 app.use(express.json());
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+    
+    console.error(error);
+    
+    return response.status(500).json({
+        status: "error",
+        message: error.message
+    });
+});
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
