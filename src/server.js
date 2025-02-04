d317805678e24cd9b86baa47191aacb373b52d3e
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-02-04 20:47:03 -0300

    Adicionada conexão de SQL Server no arquivo server.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ require("express-async-errors")
 const express = require('express');
 const AppError = require('./utils/AppError');
 const app = express();
-const PORT = 3000;
-app.use(express.json());
+const connectionSqlServer = require("../src/database/sqlserver/index")
 
+connectionSqlServer();
+
+app.use(express.json());
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
@@ -21,5 +23,6 @@ app.use((error, request, response, next) => {
     });
 });
 
-
+// Running server
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
