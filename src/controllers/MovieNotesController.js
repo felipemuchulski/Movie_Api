53162b0b97146cbdd9ea66c9cc89614883515181
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-02-06 22:00:50 -0300

    Adicionada estrutura de show, delete e index

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -2,7 +2,7 @@ const knex = require("../database/knex/knex");
 
 class MoveNotesController{
     async create(request, response){
-        const {title, description, raiting } = request.body;
+        const {title, description, raiting, tags } = request.body;
         const { id } = request.params;
 
         try {
@@ -20,6 +20,14 @@ class MoveNotesController{
                 return response.status(500).json({ error: "Failed to create note "});
           }
 
+          // Insert move_tag
+          const tagsInsert = tags.map(name => ({
+              note_id,
+              id,
+              name
+          }));
+          await knex("movie_tags").insert(tagsInsert);
+          
           console.log("ID Note: ", note_id);
           return response.status(201).json({message: "Note created"});
         } catch (error) {
@@ -27,4 +35,34 @@ class MoveNotesController{
             return response.status(500).json({ error: "Internal Server Error "});
         }
     }
+
+    async show(request, response){
+      const {id} = request.params;
+      console.log("Id recebido", id);
+
+      const note = await knex("Movie_Notes").where({id}).first();
+      const tags = await knex("tags").where({note_id: id}).orderBy("name");
+      return response.json({
+        ...note,
+        tags
+      });
+    }
+    
+    async delete(request, response){
+      const {id} = request.params;
+      await knex("Movie_Notes").where({id}).delete();
+
+      return response.json();
+    }
+
+    async index(request, respose){
+      const {id, title, tags} = request.query;
+
+      let notes;
+
+      if(tags){
+        
+      }
+    }
+
 }
