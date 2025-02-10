96f6257f539ca6347657406ab06d7b6a398db32e
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-02-10 20:48:40 -0300

    Arquivo de rotas de tags

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,9 @@
+const {Router} = require("express");
+
+const TagsController = require("../controllers/TagsController");
+const tagsRoutes = Router();
+
+const tagsController = new TagsController();
+tagsRoutes.get("/:id", tagsController.index);
+
+module.exports = tagsRoutes;
