d9ad35b94483f15beae01ff77b1dfb42cbce5e1c
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-02-10 21:57:37 -0300

    Atualizado o arquivo migrations

--- src/database/knex/migrations/20250205201316_createMoviesNotes.js
+++ src/database/knex/migrations/20250205201316_createMoviesNotes.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable("Movie_Notes", table => {
     table.increments("id");
     table.text("title");
     table.text("description");
+    table.integer("raiting");
     table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     
     table.timestamp("created_at").default(knex.fn.now());
