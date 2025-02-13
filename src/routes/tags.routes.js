799409838a77d260aa0a1c7289573c97c0f39ab4
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-02-13 19:27:05 -0300

    Realizada alteração na chamada do id do usuário

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -4,6 +4,6 @@ const TagsController = require("../controllers/TagsController");
 const tagsRoutes = Router();
 
 const tagsController = new TagsController();
-tagsRoutes.get("/:id", tagsController.index);
+tagsRoutes.get("/:user_id", tagsController.index);
 
 module.exports = tagsRoutes;
